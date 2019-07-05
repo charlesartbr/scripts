dd8a215ae5cd8635805ee7737b639b4c984bacbc
Charles Cavalcante <e-mail@charles.art.br>
2019-07-05 18:04:14 -0300

    lista os produtos

--- eucatex-export-texts.py
+++ eucatex-export-texts.py
@@ -1,6 +1,13 @@
-import sys, os, urllib.request, json
+import sys, os, urllib.request, json, re, codecs
 
+# endpoint da API
 apiEndpoint = 'https://eucatex.api.aatb.com.br/catalog/'
+
+
+# remove HTML de uma string
+def strip_html(text):
+    return re.sub('<[^<]+?>', '', text)
+
 
 # lista de categorias
 print('SELECIONE A CATEGORIA:')
@@ -10,7 +17,7 @@ with urllib.request.urlopen(apiEndpoint + 'category?language=pt') as url:
     data = json.loads(url.read().decode())
 
     for category in data:
-        print(category['categoryId'], '-', category['name'])
+        print(category['categoryId'], '-', strip_html(category['name']))
 
 
 # seleciona a categoria
@@ -25,17 +32,29 @@ print('\nINICIANDO CAPTURA DA CATEGORIA:', category['name'])
 if os.path.isdir('eucatex-export-texts') == False:
     os.mkdir('eucatex-export-texts')
 
-f = open('eucatex-export-texts/' + category['name'] + '.html', 'w+')
-
+f = codecs.open('eucatex-export-texts/' + strip_html(category['name']) + '.html', 'w+', "utf-8")
+f.write('<html><body>')
 
 # iterage nas categorias recursivamente exibindo os produtos
-def print_products(categories):
+def print_products(categories, dept = 0):
+
+    dept += 1
 
     for category in categories:
-        print(category['categoryId'], '-', category['name'])
 
+        f.write('<h' + str(dept) + '>' + category['name'] + '</h' + str(dept) + '>')
+
+        print('\nCategoria:', strip_html(category['name']))
+
+        # recursivo nas subcategorias
         if 'subCategories' in category:
-            print_products(category['subCategories'])
+            print_products(category['subCategories'], dept)
+
+        # lista os produtos
+        if 'products' in category:
+            for product in category['products']:
+                print('Produto:', strip_html(product['name']))
+                f.write('<h' + str(dept + 1) + '>' + product['name'] + '</h' + str(dept + 1) + '>')
 
 
 # retorna a lista de produtos
@@ -45,6 +64,9 @@ with urllib.request.urlopen(apiEndpoint + 'category?language=pt&products=true&ca
 
     print_products(data)
 
+# fecha o arquivo
+f.write('</body></html>')
+f.close()
 
 # sai do programa
 sys.exit()
