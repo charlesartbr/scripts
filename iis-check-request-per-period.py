63af3e8a80714b0bf31cd6f6c28ef1c6b5d22272
Charles Cavalcante <e-mail@charles.art.br>
2020-06-23 18:21:03 -0300

    check number of request per period in log files

---
+++ iis-check-request-per-period.py
@@ -0,0 +1,21 @@
+import glob
+import codecs
+
+for file in glob.glob('*/*2006*', recursive = True):
+
+    total = 0
+    r = 0
+
+    f = codecs.open(file, "r", "utf-8")
+
+    for line in f:
+
+        total += 1
+
+        if "2020-06" in line:
+            r += 1
+
+    f.close()
+
+    if quatro > 1000:
+        print(file + ": total=" + str(total) + ' - r=' + str(r) + ' %=' + ("%.2f" % (r / total * 100)))
